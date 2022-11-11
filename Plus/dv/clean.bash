f25cbea0362aa408126dfebc404c36ab79310ba7
Yuri Panchul <yuri@panchul.com>
2022-11-11 15:48:59 -0800

    Experimenting with different versions of simulation script

--- Plus/dv/clean.bash
+++ Plus/dv/clean.bash
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
-# See the decription of these settings in scripts/README.md file
-set -Eeuxo pipefail
+hex_file=code_demo.mem
 
-cd "$(dirname $0)"
-rm -rf log.txt run
+script_path="$0"
+script=$(basename "$script_path")
+script_dir=$(dirname "$script_path")
+
+error ()
+{
+  printf "$script: $*\n" 1>&2
+  exit 1
+}
+
+run_dir="$PWD"
+log="$run_dir/log.txt"
+
+cd "$script_dir" \
+  || error "cannot cd \"$script_dir\""
+
+. ../scripts/setup.source.bash
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup ()
+{
+  trap - SIGINT SIGTERM ERR
+  rm -rf log.txt run
+}
