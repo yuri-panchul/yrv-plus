c02f36e896849d81ffda5790f44e819131a1ed3e
Yuri Panchul <yuri@panchul.com>
2022-11-11 17:36:56 -0800

    Messing with scripts

--- Plus/dv/clean.bash
+++ Plus/dv/clean.bash
@@ -1,29 +1,4 @@
 #!/usr/bin/env bash
 
-hex_file=code_demo.mem
-
-script_path="$0"
-script=$(basename "$script_path")
-script_dir=$(dirname "$script_path")
-
-error ()
-{
-  printf "$script: $*\n" 1>&2
-  exit 1
-}
-
-run_dir="$PWD"
-log="$run_dir/log.txt"
-
-cd "$script_dir" \
-  || error "cannot cd \"$script_dir\""
-
-. ../scripts/setup.source.bash
-
-trap cleanup SIGINT SIGTERM ERR EXIT
-
-cleanup ()
-{
-  trap - SIGINT SIGTERM ERR
-  rm -rf log.txt run
-}
+set -ex  # Exit on non-zero status and print each command
+. "($realpath $(dirname $0))/../scripts/clean.bash"
--- Plus/dv/clean2.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# See the decription of these settings in scripts/README.md file
-set -Eeuxo pipefail
-
-cd "$(dirname $0)"
-rm -rf log.txt run
