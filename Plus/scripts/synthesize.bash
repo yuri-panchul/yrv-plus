d7217a5d4bfef768ff8e456df687f61e8a3b1256
Yuri Panchul <yuri@panchul.com>
2022-11-11 20:19:05 -0800

    Script debug

--- Plus/scripts/synthesize.bash
+++ Plus/scripts/synthesize.bash
@@ -3,10 +3,11 @@
 set -ex  # Exit on non-zero status and print each command
 
 script=$(basename $0)
-setup=scripts/setup.source.bash
+setup=../scripts/setup.source.bash
 
    [ -f ../../$setup ] && . ../../$setup \
 || [ -f    ../$setup ] && .    ../$setup \
+|| [ -f       $setup ] && .       $setup \
 || (printf "$script: cannot find $setup\n" 1>&2; exit 1)
 
 #-----------------------------------------------------------------------------
