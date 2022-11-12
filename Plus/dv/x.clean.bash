1a1376f90fdc36d4a714415bec79c91e803b9703
Yuri Panchul <yuri@panchul.com>
2022-11-11 17:45:46 -0800

    Messing with scripts

--- Plus/dv/x.clean.bash
+++ Plus/dv/x.clean.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 set -ex  # Exit on non-zero status and print each command
-. "($realpath $(dirname $0))/../scripts/x.clean.bash"
+. "$(realpath $(dirname $0))/../scripts/x.clean.bash"
