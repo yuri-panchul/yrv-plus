f1286f4a59919efcd593cebb310806b03f56b03c
Yuri Panchul <yuri@panchul.com>
2022-11-11 17:48:15 -0800

    Messing with scripts

--- Plus/scripts/x.clean.bash
+++ Plus/scripts/x.clean.bash
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
 set -ex  # Exit on non-zero status and print each command
-
-script=$(basename $0)
-setup=scripts/x.setup.source.bash
-
-   [ -f    ../$setup ] && .    ../$setup \
-|| [ -f ../../$setup ] && . ../../$setup \
-|| (printf "$script: cannot find $setup\n" 1>&2; exit 1)
-
-rm -rf run "$log"
+rm -rf run log.txt
