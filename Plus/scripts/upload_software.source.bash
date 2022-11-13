a08e9e8b38d5e5ef82e4329824d5573cdcb4208a
Yuri Panchul <yuri@panchul.com>
2022-11-13 12:17:59 -0800

    Improving memory options

--- Plus/scripts/upload_software.source.bash
+++ Plus/scripts/upload_software.source.bash
@@ -2,8 +2,8 @@
 
 program=program.mem
 
-   cp "../${hex_file:=code_demo.mem}" $program &> /dev/null \
-|| cp "$rtl_dir/$hex_file"             $program &> /dev/null \
+   cp "../${hex_file:=code_demo.mem32}" $program &> /dev/null \
+|| cp "$rtl_dir/$hex_file"              $program &> /dev/null \
 \
 || error "Cannot find \"$hex_file\""       \
          "neither in $(readlink -f ..)"    \
