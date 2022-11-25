6e7377059df2d5ebcbd69cece4633a576385c6b0
Yuri Panchul <yuri@panchul.com>
2022-11-25 11:20:39 -0800

    Create based for adding DE10-Lite port

---
+++ Plus/boards/de10_lite/top.sv
@@ -0,0 +1,39 @@
+module top
+(
+    input           adc_clk_10,
+    input           max10_clk1_50,
+    input           max10_clk2_50,
+
+    input   [ 1:0]  key,
+    input   [ 9:0]  sw,
+    output  [ 9:0]  led,
+
+    output  [ 7:0]  hex0,
+    output  [ 7:0]  hex1,
+    output  [ 7:0]  hex2,
+    output  [ 7:0]  hex3,
+    output  [ 7:0]  hex4,
+    output  [ 7:0]  hex5,
+
+    output          vga_hs,
+    output          vga_vs,
+    output  [ 3:0]  vga_r,
+    output  [ 3:0]  vga_g,
+    output  [ 3:0]  vga_b,
+
+    inout   [35:0]  gpio
+);
+
+    assign led  = 10'b0;
+
+    assign hex0 = 8'hff;
+    assign hex1 = 8'hff;
+    assign hex2 = 8'hff;
+    assign hex3 = 8'hff;
+    assign hex4 = 8'hff;
+    assign hex5 = 8'hff;
+
+    wire clk   = max10_clk1_50;
+    wire reset = sw [9];
+
+endmodule
