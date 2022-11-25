e503024a8e5567b9b444dc851eedb4d3ac503d43
Yuri Panchul <yuri@panchul.com>
2022-11-25 10:43:03 -0800

    Added comments to a script

--- Plus/scripts/dd_script.bash.template_copy_everywhere.bash
+++ Plus/scripts/dd_script.bash.template_copy_everywhere.bash
@@ -14,10 +14,24 @@ fi
 find_command="find .. -name '[0-9][0-9]_*.bash' -not -path '../scripts/*'"
 eval "$find_command"
 
-read -p "Are you sure to overwrite all the files above? " -n 1 -r
+# read:
+#
+# -n nchars return after reading NCHARS characters rather than waiting
+#           for a newline, but honor a delimiter if fewer than
+#           NCHARS characters are read before the delimiter
+#
+# -p prompt output the string PROMPT without a trailing newline before
+#           attempting to read
+#
+# -r        do not allow backslashes to escape any characters
+# -s        do not echo input coming from a terminal
+
+read -n 1 -r -p "Are you sure to overwrite all the files above? "
 printf "\n"
 
 if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+    # vargs:
+    #
     # -n max-args,  --max-args=max-args
     # -r,           --no-run-if-empty    (GNU extension)
     eval "$find_command" | xargs -n 1 -r cp dd_script.bash.template
