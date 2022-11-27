d8fcbd75a3f27a68e81fd7ba9579e3c3b2c3a8c1
Yuri Panchul <yuri@panchul.com>
2022-11-27 08:42:13 -0800

    Fixed a problem in configuration script with progress bar when config takes too long

--- Plus/scripts/06_configure_fpga.source.bash
+++ Plus/scripts/06_configure_fpga.source.bash
@@ -16,8 +16,8 @@ killall jtagd 2>/dev/null || true
 
 quartus_pgm -l &> cable_list
 
-CABLE_NAME_1=$(set +o pipefail; grep "1) " cable_list | sed 's/1) //')
-CABLE_NAME_2=$(set +o pipefail; grep "2) " cable_list | sed 's/2) //')
+CABLE_NAME_1=$(set +o pipefail; grep "1) " cable_list | sed 's/.*1) //')
+CABLE_NAME_2=$(set +o pipefail; grep "2) " cable_list | sed 's/.*2) //')
 
 if [ -n "$CABLE_NAME_1" ]
 then
